26a0b8a7eae3ecbc804beb4cbfd8893ace4b78bc
velotist <velospam@posteo.de>
2020-12-13 16:41:08 +0100

    Use SSL in API

--- DatingAppAPI/Startup.cs
+++ DatingAppAPI/Startup.cs
@@ -42,7 +42,7 @@ namespace DatingApp
 
             app.UseRouting();
 
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
