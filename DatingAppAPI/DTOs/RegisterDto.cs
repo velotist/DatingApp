5f85be63495d840e63ba03522c50a92d7cf7240f
velotist <velospam@posteo.de>
2020-12-20 18:49:42 +0100

    Add attribute required
    Add angular material
    
    - Replace approximately with compatible to version

--- DatingAppAPI/DTOs/RegisterDto.cs
+++ DatingAppAPI/DTOs/RegisterDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingAppAPI.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
