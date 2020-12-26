6658dfb4ddde09c01978e7cfa1fab5ed442de1db
velotist <velospam@posteo.de>
2020-12-26 11:03:04 +0100

    Add service

---
+++ DatingAppAPI/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using DatingApp.Entities;
+using DatingAppAPI.Interfaces;
+
+namespace DatingAppAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
